656a0a7fa09f3746c1ad2b0adb88d22ee8ed7d42
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-24 15:19:36 +0200

    add new resource

---
+++ app/Filament/Resources/CommissionsResource/Pages/CreateCommissions.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\CommissionsResource\Pages;
+
+use App\Filament\Resources\CommissionsResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCommissions extends CreateRecord
+{
+    protected static string $resource = CommissionsResource::class;
+}
---
+++ app/Filament/Resources/RentResource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\RentResource\Pages;
+use App\Models\Rent;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class RentResource extends Resource
+{
+    protected static ?string $model = Rent::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('rent')
+                    ->sortable()
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRents::route('/'),
+            'create' => Pages\CreateRent::route('/create'),
+            'edit' => Pages\EditRent::route('/{record}/edit'),
+        ];
+    }
+}
