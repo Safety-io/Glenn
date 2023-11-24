b930d4b1ebedfd05c3d833d5630e26348585c841
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-24 23:32:49 +0200

    bug serveur

--- resources/views/components/house-item.blade.php
+++ resources/views/components/house-item.blade.php
@@ -15,8 +15,6 @@
                                                 <img src="storage/{{ $image }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
                                             </div>
                                         @endforeach
-                                    @else
-                                        {{ "hrrrr" }}
                                     @endif
                                 </div>
                                 <!-- Slider indicators -->
