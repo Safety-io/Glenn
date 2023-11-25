5cc153e3c65c96dbb6b85f6be5a6445c16afc0f3
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-25 19:39:19 +0200

    bug serveur

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -16,7 +16,7 @@
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="#" class="flex items-center">
-                                <img src="{{ asset('/imgs/logo1.jpg') }}" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+                                <img src="{{ asset('/imgs/logorn.png') }}" class="mr-3 h-full w- sm:h-9" alt="Flowbite Logo" />
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">GnProperties</span>
             </a>
             <div class="flex items-center lg:order-2">
