5f3cc6374cff0c90120865f3b3227262f37fe9bb
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-25 19:27:45 +0200

    bug serveur

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -12,12 +12,11 @@
     <title>@yield('title')</title>
 </head>
 <body>
-
 <header>
     <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
         <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="#" class="flex items-center">
-                {{--                <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />  --}}
+                                <img src="{{ asset('/imgs/logo1.jpg') }}" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
                 <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">GnProperties</span>
             </a>
             <div class="flex items-center lg:order-2">
