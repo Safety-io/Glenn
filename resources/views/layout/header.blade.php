54b33588116acbaa2b70ba27dbc42704d46093b1
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-26 16:42:03 +0200

    search

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -30,13 +30,13 @@
             <div class="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                 <ul class="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                     <li>
-                        <a href="{{ route('pages.index') }}" class="block py-2 pr-4 pl-3 text-white rounded bg-blue-700 lg:bg-transparent lg:p-0 dark:text-[#f8dfbb]" aria-current="page">Home</a>
+                        <a href="{{ route('pages.index') }}" class="block py-2 pr-4 pl-3 text-white rounded  lg:bg-transparent lg:p-0 dark:text-[#f8dfbb]" aria-current="page">Home</a>
                     </li>
                     <li>
-                        <a href="{{ route('pages.about') }}" class="block py-2 pr-4 pl-3 border-gray-100 hover:bg- lg:p-0 ">About</a>
+                        <a href="{{ route('pages.about') }}" class="block py-2 pr-4 pl-3 border-gray-100  lg:p-0 ">About</a>
                     </li>
                 </ul>
-            </div>
+            </div>x
         </div>
     </nav>
 </header>
