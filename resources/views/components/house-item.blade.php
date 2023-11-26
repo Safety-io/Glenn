d38a510e800c0d9a93c77060f2b160f05587a15f
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-26 19:59:20 +0200

    search

--- resources/views/components/house-item.blade.php
+++ resources/views/components/house-item.blade.php
@@ -83,6 +83,17 @@
                                     </a>
                                     <span>Location : {{ City::find($house->city + 1)?->name ?? 'Cyprus' /*$house->city->name*/}}</span>
                                 </li>
+                                <li class="flex gap-x-3 items-center mb-3 font-medium ">
+                                    <a href="" class="mr-1 text-green-600 dark:text-gray-400">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                             fill="currentColor" class="bi bi-check-circle-fill" viewBox="0 0 16 16"
+                                             color="gray">
+                                            <path
+                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"></path>
+                                        </svg>
+                                    </a>
+                                    <span>Address : {{ $house->address}}</span>
+                                </li>
                                 <li class="flex gap-x-3 items-center mb-3 font-medium">
                                     <a href="" class="mr-1 text-green-600 dark:text-gray-400">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
