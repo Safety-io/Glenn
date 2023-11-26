034a5bbe788a36399ec204a5c766fb2abf1c5cf4
jeanluckawel <jeanluckawel45@gmail.com>
2023-11-26 21:24:24 +0200

    search

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
 <div class="w-full flex mt-8 bg-[#eee0cf] text-[#4e4539]">
     <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
-    <p class="text-sm text-center py-4 mx-auto">All rights reserved <a href="https://www.safetyio.net/"><span class="font-bold">Safety IO</span></a> </p>
+    <p class="text-sm text-center py-4 mx-auto">© 2023 copyright   <a href="https://www.safetyio.net/"><span class="font-bold">Safety IO</span></a> </p>
 </div>
