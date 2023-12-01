97258dff37c258fd6d7f8bdfc1e086d3c1b15d95
paul <paulkaf84@gmail.com>
2023-12-01 19:11:40 +0200

    fix favicon

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>About</title>
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="favicon.png">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 text-gray-700">
